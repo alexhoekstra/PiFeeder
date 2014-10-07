83a8d55fed6ff1b2eb24870d03554b9693b9da3e
alexhoekstra <aph4@students.calvin.edu>
2014-10-07 14:20:35 -0400

    testing this bad boy

--- PiFeeder.py
+++ PiFeeder.py
@@ -19,29 +19,30 @@ import RPi.GPIO as GPIO  # We need a way to send out and receive info lets use G
 import sys               # To close if the human makes an error
 
 
-#Set up the GPIO pins before we initialize them
-ServoPin=18
-ButtonPin=22
-BeeperPin=24
-PhotoPin1=17
-PhotoPin2=21
+class PiFeeder():
+  def __init(self):
+  #Set up the GPIO pins before we initialize them
+  self.ServoPin=18
+  self.ButtonPin=22
+  self.BeeperPin=24
+  self.PhotoPin1=17
+  self.PhotoPin2=21
 
 #Initialize each pin and set GPIO mode
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(ButtonPin,GPIO.IN)
-GPIO.setup(BeeperPin,GPIO.OUT)
-GPIO.setup(PhotoPin1,GPIO.IN)
-GPIO.setup(PhotoPin2,GPIO.IN)
+  GPIO.setmode(GPIO.BCM)
+  GPIO.setup(self.ButtonPin,GPIO.IN)
+  GPIO.setup(self.BeeperPin,GPIO.OUT)
+  GPIO.setup(self.PhotoPin1,GPIO.IN)
+  GPIO.setup(self.PhotoPin2,GPIO.IN)
 
 #make sure beeper is off
-GPIO.output(BeeperPin,False)
-
-
-servo = PWM.Servo()
-
-QuarterCup = .75
-FullServing = QuarterCup
-HalfServing = QuarterCup/2
+  GPIO.output(self.BeeperPin,False)
+  #init our servo
+  self.servo = PWM.Servo()
+  #Size portions for servo sleeps
+  self.QuarterCup = .75
+  self.FullServing = QuarterCup
+  self.HalfServing = QuarterCup/2
 
 ###########################################
 # serv_CounterClockwise rotates a servo 
@@ -52,14 +53,14 @@ HalfServing = QuarterCup/2
 #   so we use this only to unstick
 #   stuck bits of tasty food
 ############################################
-def serv_CounterClockwise(ServoPIN,SleepTime):
+  def serv_CounterClockwise(self,ServoPIN,SleepTime):
   # Set servo on Servo1Pin to 2000s (2.0ms)
   # rotates ccw 
   # thanks to David Bryan
-  servo.set_servo(ServoPIN, 2000)
-  time.sleep(SleepTime)
-  servo.stop_servo(ServoPIN)
-  time.sleep(.25)
+    self.servo.set_servo(ServoPIN, 2000)
+    time.sleep(self.SleepTime)
+    self.servo.stop_servo(ServoPIN)
+    time.sleep(.25)
 
 
 
@@ -73,14 +74,14 @@ def serv_CounterClockwise(ServoPIN,SleepTime):
 #   we will use this function to cater to our 
 #   overlords will.
 ############################################
-def serv_Clockwise(ServoPIN, SleepTime):
+  def serv_Clockwise(self,ServoPIN, SleepTime):
   # Set servo on ServoPin to 1200us (1.2ms)
   # This rotates the servo CW.
   # thanks to David Bryan
-  servo.set_servo(ServoPIN, 1200)
-  time.sleep(SleepTime)
-  servo.stop_servo(ServoPIN)
-  time.sleep(.25)
+    self.servo.set_servo(ServoPIN, 1200)
+    time.sleep(SleepTime)
+    self.servo.stop_servo(ServoPIN)
+    time.sleep(.25)
 
 
 #############################################
@@ -88,10 +89,10 @@ def serv_Clockwise(ServoPIN, SleepTime):
 #   announcing that our master of the 
 #   houses food is ready for his consumption
 #############################################
-def beepBoop(sleepTime):
-  GPIO.output(BeeperPin, True)
-  time.sleep(sleepTime)
-  GPIO.output(BeeperPin,False)
+  def beepBoop(sleepTime):
+    GPIO.output(self.BeeperPin, True)
+    time.sleep(sleepTime)
+    GPIO.output(self.BeeperPin,False)
 
 
 #############################################
@@ -101,35 +102,35 @@ def beepBoop(sleepTime):
 #   Humans always get it wrong so this will
 #   ensure the Cats are happy.
 #############################################
-def feedCat():
+  def feedCat(self):
   #feed the cat
-  if (GPIO.input(PhotoPin1) == True and GPIO.inpute(PhotoPin2) == True):#if empty
-    beepBoop(.15)
-    print "bowl empty"
-    time.sleep(.5)
-    serv_Clockwise(ServoPin,FullServing)
-    time.sleep(.5)
-    serv_CounterClockwise(ServoPin,.1)
-  elif (GPIO.input(PhotoPin1) == True and GPIO.input(PhotoPin2) == False):#if not empty but not full
-    beepBoop(.15)
-    print "bowl not empty"
-    time.sleep(.5)
-    serv_Clockwise(ServoPin, HalfServing)
-    time.sleep(.5)
-    serv_CounterClockwise(ServoPin,.05)
+    if (GPIO.input(self.PhotoPin1) == True and GPIO.inpute(self.PhotoPin2) == True):#if empty
+      beepBoop(.15)
+      print "bowl empty"
+      time.sleep(.5)
+      self.serv_Clockwise(self.ServoPin,self.FullServing)
+      time.sleep(.5)
+      self.serv_CounterClockwise(self.ServoPin,.1)
+    elif (GPIO.input(self.PhotoPin1) == True and GPIO.input(self.PhotoPin2) == False):#if not empty but not full
+      self.beepBoop(.15)
+      print "bowl not empty"
+      time.sleep(.5)
+      self.serv_Clockwise(self.ServoPin, self.HalfServing)
+      time.sleep(.5)
+      self.serv_CounterClockwise(self.ServoPin,.05)
   # Commented out b/c bowl isn't connected yet. this is default if bowl is not hooked up
   #elif (GPIO.input(PhotoPin1) == False and GPIO.input(PhotoPin2) == False): #cat hasn't eaten previous meal
     #print "OH NO PLEASE DONT BE DEAD"
     #beepBoop(3)
     #time.sleep(3)
     #beepBoop(3)
-  else: #assume empty
-    beepBoop(.15)
-    print "assumed bowl empty"
-    time.sleep(.5)
-    serv_Clockwise(ServoPin,FullServing)
-    time.sleep(.5)
-    serv_CounterClockwise(ServoPin,.05)
+    else: #assume empty
+      self.beepBoop(.15)
+      print "assumed bowl empty"
+      time.sleep(.5)
+      self.serv_Clockwise(self.ServoPin,self.FullServing)
+      time.sleep(.5)
+      self.serv_CounterClockwise(self.ServoPin,.05)
     
 ###############################################  
 # validHour(num) receives a number which will
@@ -140,111 +141,77 @@ def feedCat():
 #   and if it does, returns it, if not, prompts
 #   the user for a valid hour
 ###############################################    
-def validHour(num):
-  notSet = True
-  feedHour = input("What is the " + feedTime(num) + " hour you would like to feed him at? (24 hr format, no leading zeros)")
-  while notSet:  
-    if (feedHour >= 0 and feedHour <= 24):
-      notSet = False
-    else:
-      feedHour = input("I'm sorry, thats an invalid hour, please try again")
-  return feedHour      
 
-###############################################  
-# validMinute(num) receives a number which will
-#   then prompt the user with the grammatically
-#   correct version for when our overlords would
-#   like to be fed. It checks that number to
-#   see if it exists within the 60 minute clock
-#   and if it does, returns it, if not, prompts
-#   the user for a valid minute
-###############################################  
-def validMinute(num):
-  notSet = True
-  feedMinute = input("What is the " + feedTime(num) +" minute would like to feed him at? (60 min format, no leading zeros)")
-  while notSet:  
-    if (feedMinute >= 0 and feedMinute <= 60):
-      notSet = False
-    else:
-      feedMinute = input("I'm sorry, thats an invalid Minute, please try again")
-  return feedMinute 
-
-###############################################  
-# feedTime(num) receives a number and assumes
-#  if its not 1 then it must be 2 (cats don’t
-#  eat more than twice silly human) and returns
-#  the grammatically correct version of that 
-#  number
-###############################################  
-def feedTime(num):
-  if num == 1:
-    return "first"
-  else:
-    return "second"
-
-
+def get_Time(num):
+  invalidTime = True
+  while (invalidTime):
+    time = raw_input("Please enter time " + num + "seperating hour and minute by spaces (i.e 22 30)").split()
+    if int(time[0]) > 23:
+      print "invalid hour"
+    elif int(time[1]) > 59:
+      print "invalid minute"
+  return time
 
 # Ask the user if the time was set. This needs to be done
 # on a raspberry because the internal clock gets cooky
 # when off power for a short period of time
 
+def main():
+  feedTimes = raw_input("Would you like to set times to feed the cat?(y/n)")
+  feedAtTimes = False
 
-feedTimes = raw_input("Would you like to set times to feed the cat?(y/n)")
-feedAtTimes = False
-
-if feedTimes.lower() == "y":
-  timeSet = raw_input("Have you remembered to set the clock? (y/n)") 
-  noTimeSet = True
+  if feedTimes.lower() == "y":
+    timeSet = raw_input("Have you remembered to set the clock? (y/n)") 
+    noTimeSet = True
 
   # if the user set the time.. were all cool,
   # if they didn’t, we tell him what’s what and exit the program
   # if they enter a weird value in… humans do that sometimes,
   # then we ask them again
 
-  while noTimeSet:
-    if timeSet.lower() == "y":
-      noTimeSet = False
-    elif timeSet.lower() == "n":
-      print "Please set the time and run the program again"
-      print "This can be done with date —set i.e."
-      print "sudo date --set=(quote)9 AUG 2013 16:15:00(quote) "
-      sys.exit()
-    else:
-      timeSet = raw_input("I'm sorry, I didn't get that. Try again (y/n)")
+    while noTimeSet:
+      if timeSet.lower() == "y":
+        noTimeSet = False
+      elif timeSet.lower() == "n":
+        print "Please set the time and run the program again"
+        print "This can be done with date —set i.e."
+        print "sudo date --set=(quote)9 AUG 2013 16:15:00(quote) "
+        sys.exit()
+      else:
+        timeSet = raw_input("I'm sorry, I didn't get that. Try again (y/n)")
 
-  feedAtTimes = True
+    feedAtTimes = True
   # setting when our masters want to be fed.
-  feedHour1 = validHour(1)
-  feedMinute1 = validMinute(1)
-  feedHour2 = validHour(2)
-  feedMinute2 = validMinute(2)
-  print ("Feeding times are " + str(feedHour1) + ":" + str(feedMinute1) + " and " + str(feedHour2) + ":" + str(feedMinute2) + ".")
 
-#Everythings all set! lets get running!
-print "Thank you, Pifeeder is now running"
-beepBoop(.2)
-beepBoop(.2)
+    feed_time_1 = get_Time("1")
+    feed_time_2 = get_Time("2")
+    print ("Feeding times are " + feed_time_1[0] + ":" + feed_time_1[1] + " and " + feed_time_2[0] + ":" + feed_time_2[1] + ".")
 
+  #Everythings all set! lets get running!
+  print "Thank you, Pifeeder is now running"
 
+  feeder = PiFeeder()
+  feeder.beepBoop(.2)
+  feeder.beepBoop(.2)
 
-catFed = False # switch to see whether we fed the cat this minute or not
+  catFed = False # switch to see whether we fed the cat this minute or not
                # this is done so we can use the button if our cat requires
                # more food than was put out... some cats are pretty fat...
 
-# loop … forever … theres no way out of this
-while True:
-  if(GPIO.input(ButtonPin) == True):
-    print "Button Pressed"
-    feedCat()
-  if feedAtTimes:
-    if time.strftime("%H") == str(feedHour1) and time.strftime("%M") == str(feedMinute1) and catFed == False:
-      feedCat()
-      catFed = True
-    if time.strftime("%H") == str(feedHour2) and time.strftime("%M") == str(feedMinute2) and catFed == False:
-      feedCat()
-      catFed = True
-    if ((time.strftime("%M") == str(feedMinute1 + 1) or time.strftime("%M") == str(feedMinute2 +1)) and catFed == True):
-      catFed = False #reset the switch... its been a minute
+  # loop … forever … theres no way out of this
+  while True:
+    if(GPIO.input(feeder.ButtonPin) == True):
+      print "Button Pressed"
+      feeder.feedCat()
+    if feedAtTimes:
+      if time.strftime("%H") == feed_time_1[0] and time.strftime("%M") == feed_time_1[1] and catFed == False:
+        feedCat()
+        catFed = True
+      if time.strftime("%H") == feed_time_2[0] and time.strftime("%M") == feed_time_2[1] and catFed == False:
+        feedCat()
+        catFed = True
+      if ((time.strftime("%M") == str(int(feed_time_1[0])+1) or time.strftime("%M") == str(int(feed_time_2[0])+1) and catFed == True):
+        catFed = False #reset the switch... its been a minute
   
 
   
